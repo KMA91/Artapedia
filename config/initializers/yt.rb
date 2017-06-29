34667435852ec6b7dbe659768cafa56a1d4d3499
jinjiang88 <jinjiang1988@gmail.com>
2017-06-29 12:38:36 -0700

    added video model, video controller, video html

---
+++ config/initializers/yt.rb
@@ -0,0 +1,5 @@
+Yt.configure do |config|
+  config.client_id = '80357940277-1dcp5idca7081pv8hqt0i2cd3dkm86a4.apps.googleusercontent.com'
+  config.client_secret = 'dY39rMOveNNB8eTMn-688UPk'
+  config.api_key = 'AIzaSyBM1pbX-6esrUcsqY22TflQn1C1qkWSQeQ'
+end
---
+++ spec/factories/videos.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :video do
+    link "MyString"
+    title "MyString"
+    published_at "2017-06-29 19:06:15"
+    likes 1
+    dislikes 1
+    uid "MyString"
+  end
+end
