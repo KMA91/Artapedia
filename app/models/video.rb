efe566b3edadc97e43edd44e4711c068224889d3
Jake Kiernan <kiernan.jake@gmail.com>
2017-06-29 18:26:09 -0700

    debug YouTube API

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,13 @@ class Video < ActiveRecord::Base
   YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
 
   validates :link, presence: true, format: YT_LINK_FORMAT
+
+  before_save :yt_api_call
+
+  def yt_api_call
+    video = Yt::Video.new url: self.link
+    self.uid = video.id
+    self.title = video.title
+  end
+
 end
