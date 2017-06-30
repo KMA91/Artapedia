0a523d26f0441a5ea2e382d4c1f24482936e94dc
jinjiang88 <jinjiang1988@gmail.com>
2017-06-29 20:55:01 -0700

    merge

--- db/migrate/20170627205019_create_likes.rb
+++ db/migrate/20170627205019_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration
   def change
     create_table :likes do |t|
       t.references :video
-
+	  t.references :user
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170629190615) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "video_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
